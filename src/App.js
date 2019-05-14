675670b7978693c7fef6893ca3be6e8df831566f
underd0g <underd0g.3003@gmail.com>
2019-05-14 18:06:07 -0300

    adding layout and navbar components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from "./components/Home";
+import { Layout} from './components/common/Layout';
+import {NavigationBar} from "./components/common/NavigationBar";
 
 class App extends Component {
     render(){
         return (
             <React.Fragment>
-                <Router>
-                    <Switch>
-                        <Route exact path="/" component={Home} />
-                        <Route exact path="/reports" component={Income} />
-                        <Route exact path="/about" component={Home} />
-                        <Route exact path="/contact" component={contact} />
-                        <Route component={NoMatch} />
-                    </Switch>
-                </Router>
+                <NavigationBar/>
+                <Layout>
+                    <Router>
+                        <Switch>
+                            <Route exact path="/" component={Home} />
+                            {/*// <Route exact path="/reports" component={Income} />*/}
+                            {/*// <Route exact path="/about" component={Home} />*/}
+                            {/*// <Route exact path="/contact" component={contact} />*/}
+                            {/*// <Route component={NoMatch} />*/}
+                        </Switch>
+                    </Router>
+                </Layout>
             </React.Fragment>
         );
     }
---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class Home extends Component {
+
+    render() {
+        return (
+            <div>
+                sarasas
+            </div>
+        )
+    }
+}
+
+export default Home;
